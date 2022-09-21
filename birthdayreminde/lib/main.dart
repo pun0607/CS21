9f632e7976172799d070a721497e4a8cf2e3f065
Pirikan <88305721+Pirikan@users.noreply.github.com>
2022-09-21 15:28:43 +0900

    Add files via upload

--- birthdayreminde/lib/main.dart
+++ birthdayreminde/lib/main.dart
@@ -1,177 +1,170 @@
+// ignore_for_file: unused_import, duplicate_import, prefer_const_constructors, use_key_in_widget_constructors, unused_element, unused_field
+//import 'package:birthdayreminde/birthday-register.dart';
+import 'package:birthdayreminde/header/header.dart';
+import 'package:birthdayreminde/body/login.dart';
+import 'package:birthdayreminde/body/regist-account.dart';
+import 'package:birthdayreminde/body/regist-birthday.dart';
+import 'package:birthdayreminde/body/user-top.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+final _router = GoRouter(
+  routes: [
+/*    GoRoute(
+      // 1番最初の新規会員登録かログインするか選ぶページ
+      name: "welcome",
+      path: "/",
+      builder: (context, state) => MainRegisterOrLogin(),
+    ),*/
+    GoRoute(
+      // ログインページ
+      name: "login",
+      path: "/login",
+      builder: (context, state) => MainLogin(),
+    ),
+    GoRoute(
+      // 新規会員登録ページ
+      name: "registaccount",
+      path: "/registaccount",
+      builder: (context, state) => MainRegistAccount(),
+    ),
+    GoRoute(
+      // ログイン後のトップページ
+      name: "usertop",
+      path: "/user",
+      builder: (context, state) => MainUserTop(),
+      routes: [
+/*        GoRoute(
+          // ログイン後の誕生日登録ページ
+          name: "registbirthday",
+          path: "/registbirthday",
+          builder: (context, state) => MainRegistBirthday(),
+        ),*/
+/*        GoRoute(
+          // ログイン後のユーザー情報ページ
+          name: "userinfo",
+          path: "/userinfo",
+          builder: (context, state) => MainUserInfo(),
+        ),*/
+        /* 今後実装したい機能のサブルート↓ 
+        GoRoute( // 1.誕生日を登録した人の一覧リスト
+          name: "birthdaylist",
+          name: "birthdaylist",
+          path: "/birthdaylist",
+          builder: (context, state) => MainBirthdayList(),
+        ),
+        GoRoute( // 2.誕生日に登録した人、一人ひとりの詳細情報ページ
+        name: "birthdayinfo",
+        path: "/birthdayinfo",
+        builder: (context, state) => MainBirthdayInfo(),
+        ),
+        */
+      ],
+    )
+  ],
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  //このウィジェットはアプリケーションのルートです。
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Birthday reminender',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-
-        // これはアプリケーションのテーマです。
-        //
-        // "flutter run" でアプリケーションを実行してみてください。
-        // アプリケーションに青いツールバーがあることがわかります。
-        // 次に、アプリを終了せずに、下の primarySwatchを 
-        // Colors.green に変更してから、「ホット リロード」を呼び出します
-        // (「flutter run」を実行したコンソールで「r」を押すか、
-        // 単に変更を「ホット リロード」に保存します)。 Flutter IDE)。
-        // カウンターがゼロにリセットされていないことに注意してください。
-        // アプリケーションは再起動されません。
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Birthday reminender'),
-    );
+    return MaterialApp.router(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        routerDelegate: _router.routerDelegate,
+        routeInformationParser: _router.routeInformationParser,
+        debugShowCheckedModeBanner: false,
+        title: 'オメデト',
+        theme: ThemeData(
+            useMaterial3: true,
+            colorSchemeSeed: HexColor('#6fd6d2'),
+            brightness: Brightness.light,
+            textTheme: GoogleFonts.notoSansJavaneseTextTheme(
+              Theme.of(context).textTheme,
+            )
+        )
+      );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  // このウィジェットはアプリケーションのホームページです。 
-  //これはステートフルです。
-  //つまり、外観に影響を与えるフィールドを含む 
-  //State オブジェクト (以下で定義) を持っていることを意味します。
-
-  // このクラスは状態の構成です。 
-  //親 (この場合はアプリ ウィジェット) によって提供され、
-  //State のビルド メソッドによって使用される値 (この場合はタイトル) を保持します。 
-  //Widget サブクラスのフィールドは常に「final」とマークされます。
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      //このsetStateへの呼び出しは、Flutterフレームワークに何かが持っていること
-      //を伝えます。
-      //この状態で変更されました。これにより、以下の構築メソッドが再実行されます。
-      //更新された値が表示に反映されるようにします。もし私たちが
-      //setState()を呼び出さずに//_counterを呼び出すと、構築メソッドは
-      //もう一度呼び出されると、何も起こらないように見えます。
-
-
-      _counter++;
-    });
-  }
+/*class MainRegisterOrLogin extends StatelessWidget {
+  const MainRegisterOrLogin({super.key});
+  final String pagestate = "Welcome!";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-
-    // このメソッドは、たとえば上記の _incrementCounter メソッドによって
-    // 行われたように、setState が呼び出されるたびに再実行されます。
-    // Flutter フレームワークはビルド メソッドの再実行を高速化するように
-    // 最適化されているため、ウィジェットのインスタンスを個別に変更するのではなく、
-    // 更新が必要なものを再構築するだけで済みます。
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+      appBar: Header(),
+      body: RegisterOrLogin(),
+    );
+  }
+}*/
 
-        // ここで、App.build メソッドによって作成された
-        // MyHomePage オブジェクトから値を取得し、
-        // それを使用して appbar のタイトルを設定します。
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.]
-        // Center はレイアウト ウィジェットです。
-        // 1 つの子を取得し、親の中央に配置します。
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+class MainLogin extends StatelessWidget {
+  const MainLogin({super.key});
 
-          // 列もレイアウト ウィジェットです。 子のリストを取り、
-          // それらを垂直に配置します。 デフォルトでは、
-          // 子を水平方向に合わせてサイズを変更し、親と同じ高さにしようとします。
-
-           // 「デバッグ ペインティング」を呼び出します (コンソールで「p」を押し、
-           // Android Studio の Flutter Inspector の
-           //「Toggle Debug Paint」アクション、または
-           // Visual Studio Code の「Toggle Debug Paint」コマンド）を使用して、
-           // 各ウィジェットのワイヤーフレームを表示します。
-           ///
-           // 列には、それ自体のサイズと子の配置方法を制御する
-           // さまざまなプロパティがあります。
-           //  ここでは mainAxisAlignment を使用して、子を垂直方向の中央に配置します。
-           // ここでの主軸は垂直軸です。これは、列が垂直であるためです
-           // (交差軸は水平になります)。
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-        // この末尾のコンマにより、
-        // ビルド メソッドの自動フォーマットがより適切になります。  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Header(),
+      body: Login(),
     );
   }
 }
+
+class MainRegistAccount extends StatelessWidget {
+  const MainRegistAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Header(),
+      body: RegistAccount(),
+    );
+  }
+}
+
+class MainRegistBirthday extends StatelessWidget {
+  const MainRegistBirthday({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Header(),
+      body: RegistBirthday(),
+    );
+  }
+}
+
+class MainUserTop extends StatelessWidget {
+  const MainUserTop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Header(),
+      body: UserTop(),
+    );
+  }
+}
+
+/*class MainUserInfo extends StatelessWidget {
+  const MainUserInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Header(),
+      body: UserInfo(),
+    );
+  }
+}*/
